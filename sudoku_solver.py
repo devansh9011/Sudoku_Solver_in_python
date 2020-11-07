5c0295f19ab3cdd176a201b8655c02d2259382cd
Devansh Sharma <devansh9011@gmail.com>
2020-11-07 23:06:42 +0530

    made classes and added comments

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,78 +1,86 @@
-def take_input():
-    print('Enter the element of the sudoku row-wise : ')
-    # taking sudoku grid input from the user
-    for i in range(9):
-        grid.append([int(y) for y in input('Enter the ' + str(i) + ' row : ').split()])
+class sudoku:
+    def __init__(self):
+        self.grid = []
 
+    # function to take the input and form the grid
+    def take_input(self):
+        print('Enter the element of the sudoku row-wise : ')
+        # taking sudoku grid input from the user
+        for i in range(9):
+            self.grid.append([int(y) for y in input('Enter the ' + str(i) + ' row : ').split()])
 
-def print_board():
-    print('\n----------------------------')
-    for i in range(9):
-        for j in range(9):
-            if j % 3 == 0:
-                print("| ", end=' ')
-            print(grid[i][j], end=' ')
-        if i % 3 == 2:
-            print("|\n----------------------------")
-        else:
-            print("|")
+    # function to print the board
+    def print_board(self):
+        print('\n-------------------------')
+        for i in range(9):
+            for j in range(9):
+                if j % 3 == 0:
+                    print("|", end=' ')
+                print(self.grid[i][j], end=' ')
+            if i % 3 == 2:
+                print("|\n-------------------------")
+            else:
+                print("|")
 
+    # function to find a empty box in the sudoku
+    def find_empty(self):
+        for i in range(9):
+            for j in range(9):
+                if self.grid[i][j] == 0:
+                    return i, j
+        return None
 
-def find_empty():
-    for i in range(9):
-        for j in range(9):
-            if grid[i][j] == 0:
-                return i, j
-    return None
-
-
-def row_check(row, val):
-    for i in range(9):
-        if grid[row][i] == val:
-            return False
-    return True
-
-
-def col_check(row, val):
-    for i in range(9):
-        if grid[i][row] == val:
-            return False
-    return True
-
-
-def box_check(r, c, val):
-    for i in range(3):
-        for j in range(3):
-            if grid[i+r][j+c] == val:
+    # function to check whether the value 'val' is already present in the row or not
+    def row_check(self, row, val):
+        for i in range(9):
+            if self.grid[row][i] == val:
                 return False
-    return True
-
-
-def valid(val, pos):
-    row, col = pos
-    return row_check(row, val) and col_check(col, val) and box_check(int(row/3)*3, int(col/3)*3, val)
-
-
-def solve():
-    find = find_empty()
-    if find is None:
         return True
-    else:
-        row, col = find
 
-    for i in range(1, 10):
-        if valid(i, (row, col)):
-            grid[row][col] = i
-            if solve():
-                return True
-            grid[row][col] = 0
-    return False
+    # function to check whether the value 'val' is already present in the column or not
+    def col_check(self, row, val):
+        for i in range(9):
+            if self.grid[i][row] == val:
+                return False
+        return True
+
+    # function to check whether the value 'val' is already present in the 3x3 box or not
+    def box_check(self, r, c, val):
+        for i in range(3):
+            for j in range(3):
+                if self.grid[i+r][j+c] == val:
+                    return False
+        return True
+
+    # function to check whether the value 'val' can be placed in the position 'pos' or not
+    def valid(self, val, pos):
+        row, col = pos
+        return self.row_check(row, val) and self.col_check(col, val) and self.box_check(int(row/3)*3, int(col/3)*3, val)
+
+    # function to solve the sudoku
+    def solve(self):
+        find = self.find_empty()
+        if find is None:
+            return True
+        else:
+            row, col = find
+
+        for i in range(1, 10):
+            if self.valid(i, (row, col)):
+                self.grid[row][col] = i
+                if self.solve():
+                    return True
+                self.grid[row][col] = 0
+        return False
+
+    def __del__(self):
+        del self.grid
 
 
-grid = []
-take_input()
-if solve():
+s = sudoku()
+s.take_input()
+if s.solve():
     print('The solved sudoku is :\n')
-    print_board()
+    s.print_board()
 else:
     print('The sudoku is invalid and can not be solved')
